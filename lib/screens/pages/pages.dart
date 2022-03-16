cdae30275fc99f8d59a8f4a2f9712bf9d9ab285f
Manuel Duarte <carlosd99m@gmail.com>
2022-03-16 12:07:35 -0600

    refactor: reformat code

---
+++ lib/screens/pages/pages.dart
@@ -0,0 +1,11 @@
+export 'package:qr_scanner/screens/pages/home_page.dart';
+
+export 'package:qr_scanner/screens/pages/direcciones.dart';
+
+export 'package:qr_scanner/screens/pages/image/scan_image.dart';
+
+export 'package:qr_scanner/screens/pages/image/scan.dart';
+
+export 'package:qr_scanner/screens/pages/mapa/mapa_page.dart';
+
+export 'package:qr_scanner/screens/pages/mapa/mapas_page.dart';
